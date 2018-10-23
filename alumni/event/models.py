8e8e0e1c41fa34c25c0242860cacd8ac738ed83d
kunal097 <kunalsharma19997@gmail.com>
2018-10-23 14:28:51 +0530

    add model

--- alumni/event/models.py
+++ alumni/event/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User as AuthUser
 
 # Create your models here.
+
+gender = (
+	('m','male'),
+	('f','female'),
+	)
+
+
+
+class User(models.Model):
+	user=models.ForeignKey(AuthUser,on_delete=models.CASCADE)
+	name = models.CharField(max_length=20)
+	gender = models.CharField(max_length=7,choices=gender)
+	branch = models.CharField(max_length=15)
+	paasout_year = models.DateField()
+	dob = models.DateField()
+	address = models.CharField(max_length=50)
+	contact_number = models.CharField(max_length=15)
+	is_admin = models.BooleanField()
+
+
+
+	def __str__(self):
+		return self.name
